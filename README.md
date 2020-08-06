73b86b3aca95f6da759b6a8d0ebe46dfbf60188d
abaronikian <baroniki@usc.edu>
2020-08-06 03:10:55 -0700

    Add UI

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Colonist.io Card Tracker
-This extension automatically tracks and displays all resource cards held by each player in colonist.io.
+This extension automatically tracks and displays all resource cards held by each player in colonist.io, excluding cards stolen by the robber and trades since they currently cannot be tracked.
--- content.js
+++ content.js
@@ -27,14 +27,7 @@ setInterval(function() { check_in_game() }, 3000)
 let tracker_data = []
 
 function create_tracker() {
-    let tracker_element_container = document.createElement('div')
-    tracker_element_container.id = 'tracker-container'
-    tracker_element_container.innerHTML = 'Cards'
-
     get_players()
-
-    document.querySelector('body').appendChild(tracker_element_container)
-
     track_cards()
 }
 
@@ -65,6 +58,7 @@ function update_tracker() {
         chat_log_message = chat_log.children[++card_chat_index]
     }
 
+    updateUI()
     console.log(tracker_data)
 }
 
@@ -229,3 +223,167 @@ function check_in_game() {
         create_tracker()
     }
 }
+
+// ui stuff
+
+function updateUI() {
+    if(document.querySelector('.tracker-container'))
+        document.querySelector('.tracker-container').remove()
+
+    const ui_container = document.createElement('div')
+    ui_container.className = 'tracker-container'
+
+    const ui_move = document.createElement('div')
+    ui_move.className = 'move'
+    ui_move.addEventListener('dblclick', toggle_info)
+    
+
+    const ui_header = document.createElement('div')
+    ui_header.className = 'tracker-header'
+    ui_header.innerText = 'Card Tracker'
+
+    const ui_howto = document.createElement('div')
+    ui_howto.className = 'howto'
+    ui_howto.innerText = 'Drag top to move, double click to hide/show'
+
+    ui_container.appendChild(ui_move)
+    ui_container.appendChild(ui_header)
+    ui_container.appendChild(ui_howto)
+
+    const ui_info_container = document.createElement('div')
+    ui_info_container.id = 'info-container'
+    
+    let ui_card_count
+
+    for(const player of tracker_data) {
+        console.log(player)
+        const ui_player_name = document.createElement('div')
+        ui_player_name.className = 'player-name'
+        ui_player_name.innerText = player.name
+
+        const ui_player_info = document.createElement('div')
+        ui_player_info.className = 'player-info'
+
+        const ui_wood_card = document.createElement('img')
+        ui_wood_card.className = 'lobby-chat-text-icon'
+        ui_wood_card.src = '../dist/images/card_lumber.svg?v87.1'
+        ui_wood_card.alt = 'lumber'
+        ui_wood_card.height = 20
+        ui_wood_card.width = 14.25
+
+        const ui_brick_card = document.createElement('img')
+        ui_brick_card.className = 'lobby-chat-text-icon'
+        ui_brick_card.src = '../dist/images/card_brick.svg?v87.1'
+        ui_brick_card.alt = 'brick'
+        ui_brick_card.height = 20
+        ui_brick_card.width = 14.25
+
+        const ui_sheep_card = document.createElement('img')
+        ui_sheep_card.className = 'lobby-chat-text-icon'
+        ui_sheep_card.src = '../dist/images/card_wool.svg?v87.1'
+        ui_sheep_card.alt = 'wool'
+        ui_sheep_card.height = 20
+        ui_sheep_card.width = 14.25
+
+        const ui_wheat_card = document.createElement('img')
+        ui_wheat_card.className = 'lobby-chat-text-icon'
+        ui_wheat_card.src = '../dist/images/card_grain.svg?v87.1'
+        ui_wheat_card.alt = 'grain'
+        ui_wheat_card.height = 20
+        ui_wheat_card.width = 14.25
+
+        const ui_ore_card = document.createElement('img')
+        ui_ore_card.className = 'lobby-chat-text-icon'
+        ui_ore_card.src = '../dist/images/card_ore.svg?v87.1'
+        ui_ore_card.alt = 'ore'
+        ui_ore_card.height = 20
+        ui_ore_card.width = 14.25
+
+        ui_card_count = document.createElement('span')
+        ui_card_count.className = 'card-count'
+        ui_card_count.innerText = ': ' + player.cards.wood
+
+        ui_player_info.appendChild(ui_wood_card)
+        ui_player_info.appendChild(ui_card_count)
+
+        ui_card_count = document.createElement('span')
+        ui_card_count.className = 'card-count'
+        ui_card_count.innerText = ': ' + player.cards.brick
+
+        ui_player_info.appendChild(ui_brick_card)
+        ui_player_info.appendChild(ui_card_count)
+
+        ui_card_count = document.createElement('span')
+        ui_card_count.className = 'card-count'
+        ui_card_count.innerText = ': ' + player.cards.sheep
+
+        ui_player_info.appendChild(ui_sheep_card)
+        ui_player_info.appendChild(ui_card_count)
+
+        ui_card_count = document.createElement('span')
+        ui_card_count.className = 'card-count'
+        ui_card_count.innerText = ': ' + player.cards.wheat
+
+        ui_player_info.appendChild(ui_wheat_card)
+        ui_player_info.appendChild(ui_card_count)
+
+        ui_card_count = document.createElement('span')
+        ui_card_count.className = 'card-count'
+        ui_card_count.innerText = ': ' + player.cards.ore
+
+        ui_player_info.appendChild(ui_ore_card)
+        ui_player_info.appendChild(ui_card_count)
+
+
+        ui_info_container.appendChild(ui_player_name)
+        ui_info_container.appendChild(ui_player_info)
+    }
+
+    ui_container.appendChild(ui_info_container)
+
+    document.querySelector('body').appendChild(ui_container)
+
+    dragElement(ui_container)
+    
+}
+
+function toggle_info() {
+    const info = document.querySelector('#info-container')
+    if(info.style.display === 'block') {
+        info.style.display = 'none'
+        document.querySelector('.tracker-container').style.height = 'auto'
+    }
+    else {
+        info.style.display = 'block'
+    }
+}
+
+function dragElement(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    document.querySelector(".move").onmousedown = dragMouseDown;
+  
+    function dragMouseDown(e) {
+      e = e || window.event;
+      e.preventDefault();
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      document.onmouseup = closeDragElement;
+      document.onmousemove = elementDrag;
+    }
+  
+    function elementDrag(e) {
+      e = e || window.event;
+      e.preventDefault();
+      pos1 = pos3 - e.clientX;
+      pos2 = pos4 - e.clientY;
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+      elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+  
+    function closeDragElement() {
+      document.onmouseup = null;
+      document.onmousemove = null;
+    }
+  }
